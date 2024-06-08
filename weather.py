9b778f4d85c2dc32ab52fed6d0943f523d223b5f
Edmond Ina <sarerrdy4live@live.co.uk>
2024-06-08 04:18:05 +0100

    second commit

--- weather.py
+++ weather.py
@@ -5,18 +5,35 @@ from pprint import pprint
 
 load_dotenv()
 
-def get_current_weather():
-	print('\n*** Get Current Weather conditions ***\n')
+def get_current_weather(city = "Lagos"):
 
+	"""print('\n*** Get Current Weather conditions ***\n')
 	city = input("\nPlease enter a city name:\n")
+
 	request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 	#print(request_url)
-
 	weather_data = requests.get(request_url).json()
-	print(f'\nCurrent weather for {weather_data["name"]}')
+	 print(f'\nCurrent weather for {weather_data["name"]}')
 	print(f'\nThe temperature is {weather_data["main"]["temp"]}')
-	print(f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.')
+	print(f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.') """
+
+	
+	
+	request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+	weather_data = requests.get(request_url).json()
+
+	return weather_data
+
 
 
 if __name__ == "__main__":
-	get_current_weather()
+	print('\n*** Get Current Weather conditions ***\n')
+	city = input("\nPlease enter a city name:\n")
+
+	#Check for empty strings
+	if not bool(city.strip()):
+		city = "Lagos"
+		
+	weather_data = get_current_weather(city)
+	print("\n")
+	pprint(weather_data)
